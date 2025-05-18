e82116d5fb24797fe693d95d7f120a9c884db06b
DanTheWizard <139829191+DanTheWizard@users.noreply.github.com>
2025-05-18 14:12:31 -0400

    Variables now have documentation that can be read whenever you hover over them anywhere in the script (unsing Pycharm)

--- config.py
+++ config.py
@@ -18,46 +18,46 @@ Most can be overridden with override.ini for debugging
 load_dotenv(dotenv_path)                                                   # Load the variables from the .env file
 
 
-IconPath                  = WinPath(r"%public%\\Icons\\")       # Root Dir for storing Icons
-WarnIconPath              = f"{IconPath}warn.png"               # Warning/Exclamation icon
-GoodIconPath              = f"{IconPath}good.png"               # Good/Checkmark icon
-ErrorIconPath             = f"{IconPath}error.png"              # Error/X icon
+IconPath                  = WinPath(r"%public%\\Icons\\")       ;"Root Dir location for storing Icons"
+WarnIconPath              = f"{IconPath}warn.png"               ;"Warning/Exclamation icon path"
+GoodIconPath              = f"{IconPath}good.png"               ;"Good/Checkmark icon path"
+ErrorIconPath             = f"{IconPath}error.png"              ;"Error/X icon path"
 
-self_exe                  = os.path.basename(sys.executable)    # Grab the current exe name
+self_exe                  = os.path.basename(sys.executable)    ;"Grab the current focused exe title as string"
 
 
 # Machine Information
-HOSTNAME                  = socket.gethostname()                # PC hostname
-MACHINE_ID                = get_short_fingerprint()             # PC 10 digit unique hash
-USERNAME                  = get_logged_in_username()            # Current logged-in user
+HOSTNAME                  = socket.gethostname()                ;"PC hostname"
+MACHINE_ID                = get_short_fingerprint()             ;"PC 10 digit unique hash"
+USERNAME                  = get_logged_in_username()            ;"Current windows logged-in user"
 
 
 # Timeout settings
-PUBLISH_TIMEOUT           = get_override("PUBLISH_TIMEOUT",          4,  int)        # Timout in seconds before publishing PC data | Default: 4
-KILL_LOOP_TIMEOUT         = get_override("KILL_LOOP_TIMEOUT",        4,  int)        # Timeout in seconds before running the kill loop | Default: 4
-DEFAULT_SHUTDOWN_TIMEOUT  = get_override("DEFAULT_SHUTDOWN_TIMEOUT", 60, int)        # Shutdown Timeout in seconds when receiving the shutdown command | Default: 60
+PUBLISH_TIMEOUT           = get_override("PUBLISH_TIMEOUT",          4,  int)        ;"Timout in seconds before publishing PC data | Default: 4"
+KILL_LOOP_TIMEOUT         = get_override("KILL_LOOP_TIMEOUT",        4,  int)        ;"Timeout in seconds before running the kill loop | Default: 4"
+DEFAULT_SHUTDOWN_TIMEOUT  = get_override("DEFAULT_SHUTDOWN_TIMEOUT", 60, int)        ;"Shutdown Timeout in seconds when receiving the shutdown command | Default: 60"
 
 
 # Debugging (I do not recommend enabling all of them at once)
-DEBUG                     = get_override("DEBUG",          False, bool)              # Debug Mode (simple lines) | Default: False
-DEBUG_PUBLISH             = get_override("DEBUG_PUBLISH",  False, bool)              # Enable Print out of the Publishing Data to the WebSocket Server | Default: False
-DEBUG_STATUS              = get_override("DEBUG_STATUS",   False, bool)              # Enable Print out of the Receiving Status | Default: False
-DEBUG_KILL                = get_override("DEBUG_KILL",     False, bool)              # Enable Print out of the Simple killing app | Default: False
-DEBUG_KILL_ADV            = get_override("DEBUG_KILL_ADV", False, bool)              # Enable Print out of the advanced killing information | Default: False
-DEBUG_WS_MES              = get_override("DEBUG_WS_MES",   False, bool)              # Enable Print out of some extra details when receiving data from WS Server (Adv) | Default: False
-DEBUG_WS_CON              = get_override("DEBUG_WS_CON",   False, bool)              # Enable Print out of some extra connection details when connecting to the WS server (Adv) | Default: False
+DEBUG                     = get_override("DEBUG",          False, bool)              ;"Debug Mode (simple lines) | Default: False"
+DEBUG_PUBLISH             = get_override("DEBUG_PUBLISH",  False, bool)              ;"Enable Print out of the Publishing Data to the WebSocket Server | Default: False"
+DEBUG_STATUS              = get_override("DEBUG_STATUS",   False, bool)              ;"Enable Print out of the Receiving Status | Default: False"
+DEBUG_KILL                = get_override("DEBUG_KILL",     False, bool)              ;"Enable Print out of the Simple killing app | Default: False"
+DEBUG_KILL_ADV            = get_override("DEBUG_KILL_ADV", False, bool)              ;"Enable Print out of the advanced killing information | Default: False"
+DEBUG_WS_MES              = get_override("DEBUG_WS_MES",   False, bool)              ;"Enable Print out of some extra details when receiving data from WS Server (Adv) | Default: False"
+DEBUG_WS_CON              = get_override("DEBUG_WS_CON",   False, bool)              ;"Enable Print out of some extra connection details when connecting to the WS server (Adv) | Default: False"
 
 
 # WebSocket connection details
 # Using .env to not leak confidential data :)
-WS_SERVER                 = os.getenv("WS_SERVER")           # Websocket Server Location
-WS_PORT                   = int(os.getenv("WS_PORT"))        # Websocket Port
-WS_USERNAME               = os.getenv("WS_USERNAME")         # Websocket Username Authentication
-WS_PASSWORD               = os.getenv("WS_PASSWORD")         # Websocket Password Authentication
+WS_SERVER                 = os.getenv("WS_SERVER")           ;"Websocket Server Location"
+WS_PORT                   = int(os.getenv("WS_PORT"))        ;"Websocket Port"
+WS_USERNAME               = os.getenv("WS_USERNAME")         ;"Websocket Username Authentication"
+WS_PASSWORD               = os.getenv("WS_PASSWORD")         ;"Websocket Password Authentication"
 
-WS_USE_TLS                = get_override("WS_USE_TLS",               True,  bool)    # Whether to use TLS based from the server | Default: True
-WS_SHOW_CON_TOAST         = get_override("WS_SHOW_CON_TOAST",        False, bool)    # Show a notification that Connecting to server was a success | Default: False
-SHOW_WS_URL_PORT_STARTUP  = get_override("SHOW_WS_URL_PORT_STARTUP", False, bool)    # Show the WebSocket url:port on the main startup after connecting successfully | Default: False
+WS_USE_TLS                = get_override("WS_USE_TLS",               True,  bool)    ;"Whether to use TLS (SSL) connection when connecting to the WebSocket Server | Default: True"
+WS_SHOW_CON_TOAST         = get_override("WS_SHOW_CON_TOAST",        False, bool)    ;"Show a notification that Connecting to server was a success | Default: False"
+SHOW_WS_URL_PORT_STARTUP  = get_override("SHOW_WS_URL_PORT_STARTUP", False, bool)    ;"Show the WebSocket url:port on the main startup after connecting successfully | Default: False"
 
 
 if __name__ == "__main__":
